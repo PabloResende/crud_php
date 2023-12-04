aa72425a98b59a54f3be7fc9e8c6c8704584fb78
Sr.Carrilho <126430073+PabloResende@users.noreply.github.com>
2023-12-04 13:10:54 -0300

    ...

--- listar_usuario.php
+++ listar_usuario.php
@@ -1,1 +1,29 @@
 <h1>Listar Usuário</h1>
+<?php
+$sql = "SELECT * FROM cadastro";
+
+$res = $conn->query($sql);
+
+$qtd = $res->num_rows;
+
+if ($qtd > 0) {
+    echo "<table class='table table-hover table-striped table-bordered'>";
+    echo "<tr>";
+    echo "<th>#</th>";
+    echo "<th>Nome</th>";
+    echo "<th>Email</th>";
+    echo "<th>Nascimento</th>";
+    echo "</tr>";
+    while ($row = $res->fetch_object()) {
+        echo "<tr>";
+        echo "<td>" . $row->id . "</td>";
+        echo "<td>" . $row->nome . "</td>";
+        echo "<td>" . $row->email . "</td>";
+        echo "<td>" . $row->data_nasc . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<p class='alert alert-danger'>Não encontrou resultado</p>";
+}
+?>
--- salvar_usuario.php
+++ salvar_usuario.php
@@ -3,12 +3,20 @@ switch ($_REQUEST["acao"]) {
     case 'cadastrar';
         $nome = $_POST["nome"];
         $email = $_POST["email"];
-        $senha = $_POST["senha"];
+        $senha = md5($_POST["senha"]);
         $dara_nasc = $_POST["data_nasc"];
 
         $sql = "INSERT INTO cadastro (nome, email, senha, data_nasc) VALUES ('{$nome}','{$email}','{$senha}','{$dara_nasc}')";
 
         $res = $conn->query($sql);
+
+        if ($res == true) {
+            echo "<script>alert('Cadastro feito com sucesso');</script>";
+            echo    "<script>location.href='?page=listar';</script>";
+        } else {
+            echo "<script>Não foi possível cadastrar;</script>";
+            echo "<script>location.href=?page=listar;</script>";
+        }
         break;
     case 'editar';
         //code...
