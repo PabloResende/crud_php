88bd1702b3b66d12c37c132779e713b38f5a1a26
Sr.Carrilho <126430073+PabloResende@users.noreply.github.com>
2023-12-04 15:22:35 -0300

    ...

--- salvar_usuario.php
+++ salvar_usuario.php
@@ -1,44 +1,47 @@
 <?php
-switch ($_REQUEST["acao"]) {
-    case 'cadastrar';
-        $nome = $_POST["nome"];
-        $email = $_POST["email"];
-        $senha = md5($_POST["senha"]);
-        $dara_nasc = $_POST["data_nasc"];
+if (isset($_REQUEST["acao"])) {
+    $acao = $_REQUEST["acao"];
 
-        $sql = "INSERT INTO cadastro (nome, email, senha, data_nasc) VALUES ('{$nome}','{$email}','{$senha}','{$dara_nasc}')";
+    switch ($acao) {
+        case 'cadastrar':
+            $nome = $_POST["nome"];
+            $email = $_POST["email"];
+            $senha = md5($_POST["senha"]);
+            $dara_nasc = $_POST["data_nasc"];
 
-        $res = $conn->query($sql);
+            $sql = "INSERT INTO cadastro (nome, email, senha, data_nasc) VALUES ('{$nome}','{$email}','{$senha}','{$dara_nasc}')";
 
-        if ($res == true) {
-            echo "<script>alert('Cadastro feito com sucesso');</script>";
-            echo    "<script>location.href='?page=listar';</script>";
-        } else {
-            echo "<script>Não foi possível cadastrar;</script>";
-            echo "<script>location.href=?page=listar;</script>";
-        }
-        break;
+            $res = $conn->query($sql);
+
+            if ($res == true) {
+                echo "<script>alert('Cadastro feito com sucesso');</script>";
+                echo "<script>location.href='?page=listar';</script>";
+            } else {
+                echo "<script>alert('Não foi possível cadastrar');</script>";
+                echo "<script>location.href='?page=listar';</script>";
+            }
+            break;
         case 'editar':
             $nome = $_POST["nome"];
             $email = $_POST["email"];
             $senha = md5($_POST["senha"]);
             $data_nasc = $_POST["data_nasc"];
-    
+
             if (isset($_REQUEST["id"])) {
                 $id = $_REQUEST["id"];
-    
+
                 $sql = "UPDATE cadastro SET
                     nome = ?,
                     email = ?,
                     senha = ?,
                     data_nasc = ?
                 WHERE id = ?";
-    
+
                 $stmt = $conn->prepare($sql);
-    
+
                 // Bind parameters
                 $stmt->bind_param("ssssi", $nome, $email, $senha, $data_nasc, $id);
-    
+
                 // Executar a consulta
                 if ($stmt->execute()) {
                     echo "<script>alert('Operação concluída com sucesso');</script>";
@@ -48,11 +51,31 @@ switch ($_REQUEST["acao"]) {
             } else {
                 echo "<script>alert('ID não fornecido na requisição');</script>";
             }
-    
+
             echo "<script>location.href='?page=listar';</script>";
             break;
-    
         case 'excluir':
-            // Código de exclusão
+            if (isset($_REQUEST["id"])) {
+                $id = $_REQUEST["id"];
+
+                $sql = "DELETE FROM cadastro WHERE id = $id";
+
+                $res = $conn->query($sql);
+
+                if ($res) {
+                    echo "<script>alert('Excluído com sucesso');</script>";
+                } else {
+                    echo "<script>alert('Não foi possível completar a operação');</script>";
+                }
+            } else {
+                echo "<script>alert('ID não fornecido na requisição');</script>";
+            }
+            break;
+            default:
+            echo "<script>alert('Ação desconhecida: $acao');</script>";
             break;
     }
+} else {
+    echo "<script>alert('Ação não fornecida na requisição');</script>";
+}
+?>
