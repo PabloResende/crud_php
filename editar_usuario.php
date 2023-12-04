eda8317a0f54e9ae7eaa524f69c9b582a08e2ffa
Sr.Carrilho <126430073+PabloResende@users.noreply.github.com>
2023-12-04 14:21:53 -0300

    ...

--- editar_usuario.php
+++ editar_usuario.php
@@ -8,22 +8,22 @@ $row = $res->fetch_object();
 ?>
 
 <form action="?page=salvar" method="POST">
-    <input type="hidden" name="acao" value="cadastrar">
+    <input type="hidden" name="acao" value="editar" value="<?php echo  $row->id; ?>">
     <div class="mb-3">
         <label>Nome</label>
         <input type="text" name="nome" class="form-control" value="<?php echo $row->nome; ?>">
     </div>
     <div class="mb-3">
         <label>Email</label>
-        <input type="email" name="email" class="form-control">
+        <input type="email" name="email" class="form-control" value="<?php echo $row->email; ?>">
     </div>
     <div class="mb-3">
         <label>Senha</label>
-        <input type="password" name="senha" class="form-control">
+        <input placeholder="Digite a senha" type="password" name="senha" class="form-control" required>
     </div>
     <div class="mb-3">
         <label>Data de Nascimento</label>
-        <input type="text" name="data_nasc" class="form-control">
+        <input type="text" name="data_nasc" class="form-control" value="<?php echo $row->data_nasc; ?>">
     </div>
     <div class="mb-3">
         <button type="submit" class="btn btn-primary">Enviar</button>
